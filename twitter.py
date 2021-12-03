e0b63d4ed1b0d998d3389bb95dbd473b3054f08c
Robi Alisandi <robbiealisandi@gmail.com>
2021-12-03 08:56:41 +0700

    add env

--- twitter.py
+++ twitter.py
@@ -6,12 +6,13 @@ from requests_oauthlib import OAuth1
 import requests
 import os
 import re
+from dotenv import load_dotenv
 
 class Twitter:
     def __init__(self):
         print("initializing twitter....")
-        self.inits = tweepy.OAuthHandler(constants.CONSUMER_KEY, constants.CONSUMER_SCRET)
-        self.inits.set_access_token(constants.ACCESS_KEY, constants.ACCESS_SECRET)
+        self.inits = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SCRET'])
+        self.inits.set_access_token(os.environ['ACCESS_KEY'], os.environ['ACCESS_SECRET'])
         self.api = tweepy.API(self.inits)
 
 
@@ -67,10 +68,10 @@ class Twitter:
 
     def post_tweet_with_media(self, tweet, media_url, id):
         arr = str(media_url).split('/')
-        auth = OAuth1(client_key= constants.CONSUMER_KEY,
-                      client_secret= constants.CONSUMER_SCRET,
-                      resource_owner_secret= constants.ACCESS_SECRET,
-                      resource_owner_key= constants.ACCESS_KEY)
+        auth = OAuth1(client_key= os.environ['CONSUMER_KEY'],
+                      client_secret= os.environ['CONSUMER_SCRET'],
+                      resource_owner_secret= os.environ['ACCESS_SECRET'],
+                      resource_owner_key= os.environ['ACCESS_KEY'])
         r = requests.get(media_url, auth = auth)
         with open(arr[9], 'wb') as f:
             f.write(r.content)
