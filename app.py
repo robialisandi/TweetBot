f279106c4e661db21ef1415284cfecb94dc277d6
Robi Alisandi <robbiealisandi@gmail.com>
2022-01-17 15:15:27 +0700

    Add auto cut 270

--- app.py
+++ app.py
@@ -9,15 +9,21 @@ def start():
     print("Starting program...")
     dms = list()
     while True:
-        if len(dms) != 0:
+        if len(dms) is not 0:
             for i in range(len(dms)):
                 message = dms[i]['message']
                 sender_id = dms[i]['sender_id']
                 id = dms[i]['id']
-                if len(message) != 0 and len(message) < 280:
+                if len(message) is not 0 and len(message) < 280:
                     if "/gt" in message:
-                        message = message.replace("/gt", "src:asgar!")
-                        if len(message) != 0:
+                        message = message.replace("/gt", "")
+                        message = "Asgar! " + message
+                        if len(message) > 280 :
+                            start = 270
+                            stop = len(message) - 1
+                            if len(message) > stop :
+                                message = message[0: start:] + message[stop + 1::] + "~mis"
+                        if len(message) is not 0:
                             if dms[i]['media'] is None:
                                 print("DM will be posted")
                                 tw.post_tweet(message)
